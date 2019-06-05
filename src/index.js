31653cf61d5fd8c4d3ee844a7f796b0537ef6077
NdumisoMagunga <magunga55@gmail.com>
2019-06-05 08:51:04 -0700

    the funtional components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import CommentDetails from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetails
+                author="Ndumiso"
+                timeAgo="Today at 4:45PM"
+                profilePic={faker.image.avatar()}
+                blogPost="Today we are doing a very great job"
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetails
+                author="Goodman"
+                timeAgo="Today at 10:45PM"
+                profilePic={faker.image.avatar()}
+                blogPost="iReally love this way of codding"
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetails
+                author="focus"
+                timeAgo="yesterday at 4:45PM"
+                profilePic={faker.image.avatar()}
+                blogPost="we will enjoy the rest of our coding if it all looks this simple"
+                />
+            </ApprovalCard>
+            
+            
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
